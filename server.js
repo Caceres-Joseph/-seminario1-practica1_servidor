de1c7e0fbf85c5fc3ecc7751a428e3d7e404f981
Joseph <josephccaceres@gmail.com>
2019-03-14 00:43:08 -0600

    s3

--- server.js
+++ server.js
@@ -16,28 +16,72 @@ var mc = mysql.createConnection({
 });
 
 // subir imagen
-
 const fileUpload = require('express-fileupload');
+
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+
+
+
 
 //cors
 app.use(cors())
 app.use(fileUpload());
 
 app.post('/upload', function (req, res) {
- 
+
     if (Object.keys(req.files).length == 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let sampleFile = req.files.file;  
-    //return;
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv('./app/public/' + sampleFile.name, function (err) {
-        if (err)
-            return res.status(500).send(err);
-        res.send(sampleFile.name);
-    });
+    let sampleFile = req.files.file;
+
+    /*
+    +------------
+    | Guardando
+    +------------
+    */
+
+    // Create unique bucket name
+    var bucketName = uuid.v4();
+    // Create name for uploaded object key
+    var keyName = sampleFile.name
+    // Create a promise on S3 service object
+    var bucketPromise = new AWS.S3({
+        apiVersion: '2006-03-01'
+    }).createBucket({
+        Bucket: bucketName
+    }).promise();
+    // Handle promise fulfilled/rejected states
+    bucketPromise.then(
+        function (data) {
+            // Create params for putObject call
+            var objectParams = {
+                Bucket: bucketName,
+                Key: keyName,
+                Body: sampleFile.data,
+                ACL: 'public-read'
+            };
+            // Create object upload promise
+            var uploadPromise = new AWS.S3({
+                apiVersion: '2006-03-01'
+            }).putObject(objectParams).promise();
+            uploadPromise.then(
+                function (data) {
+                    console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+                });
+        }).catch(
+        function (err) {
+            console.error(err, err.stack);
+        });
+
+    console.log("Se guard el bucket jejejeje");
+
+
+    res.send(bucketName + "/" + keyName);
+
 });
 
 // connect to database
